6b821829b522b52353dc6fedf91dd5bdca8c1dfa
ssitwell <ssitwell@gmail.com>
2014-10-14 00:21:17 -0400

    Have our ffmpeg repo also be the upstream repo. Use android ndk folder in compile scripts.

--- init-android.sh
+++ init-android.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
+IJK_FFMPEG_UPSTREAM=https://github.com/petbot/ijk-ffmpeg.git
 IJK_FFMPEG_FORK=https://github.com/petbot/ijk-ffmpeg.git
 IJK_FFMPEG_COMMIT=ijk-r0.1.2-dev
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
@@ -11,13 +11,12 @@ TOOLS=tools
 echo "== pull ffmpeg base =="
 sh $TOOLS/pull-repo-base.sh $IJK_FFMPEG_UPSTREAM $IJK_FFMPEG_LOCAL_REPO
 
-function pull_fork()
-{
-    echo "== pull ffmpeg fork $1 =="
-    sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK android/ffmpeg-$1 ${IJK_FFMPEG_LOCAL_REPO}
-    cd android/ffmpeg-$1
-    git checkout ${IJK_FFMPEG_COMMIT}
-    cd -
+function pull_fork(){
+	echo "== pull ffmpeg fork $1 =="
+	sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK android/ffmpeg-$1 ${IJK_FFMPEG_LOCAL_REPO}
+	cd android/ffmpeg-$1
+	git checkout ${IJK_FFMPEG_COMMIT}
+	cd -
 }
 
 pull_fork "armv7a"
